0146a328d5669366c7951f7ba69afad5e9e74874
Surya <yourmail@gmail.com>
2026-03-15 22:53:22 +0530

    Add optimal string multiplication using vector carry simulation

---
+++ Strings/multiply_strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n = num1.size();
+        int m = num2.size();
+
+        vector<int> res(n + m, 0);
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+
+                int sum = mul + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        string ans = "";
+
+        for(int x : res){
+            if(!(ans.empty() && x == 0))
+                ans += to_string(x);
+        }
+
+        return ans.empty() ? "0" : ans;
+    }
+};
