50f8bb99117dd7d8642c5643e93f7da1012d5fa2
Surya <yourmail@gmail.com>
2026-03-16 17:20:22 +0530

    Add recursive solution to reverse a singly linked list

--- Strings/multiply_strings.cpp
+++ Strings/multiply_strings.cpp
@@ -26,4 +26,68 @@ public:
 
         return ans.empty() ? "0" : ans;
     }
+};#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Definition of singly linked list
+*/
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
 };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+
+        ListNode *newh = reverseList(head->next);
+
+        ListNode *p = head->next;
+        p->next = head;
+        head->next = NULL;
+
+        return newh;
+    }
+};
+
+int main() {
+    // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution obj;
+    head = obj.reverseList(head);
+
+    // Print reversed list
+    ListNode* temp = head;
+    while(temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
